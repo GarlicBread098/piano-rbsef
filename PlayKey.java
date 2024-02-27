d6523f1ea60c8b353432a8494e06631661473e87
GarlicBread098 <160778373+GarlicBread098@users.noreply.github.com>
2024-02-27 16:18:15 -0500

    Add files via upload

---
+++ PlayKey.java
@@ -0,0 +1,34 @@
+package thirdprojects;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
+import javax.sound.sampled.AudioInputStream;
+import javax.sound.sampled.AudioSystem;
+import javax.sound.sampled.Clip;
+import javax.sound.sampled.LineUnavailableException;
+import javax.sound.sampled.UnsupportedAudioFileException;
+
+public class PlayKey {
+	public PlayKey() {
+	
+	}
+	
+	public synchronized void playSound(String str) throws FileNotFoundException, UnsupportedAudioFileException, IOException, LineUnavailableException {
+		  new Thread(new Runnable() {
+		  
+		    public void run() {
+		      try {
+		        Clip clip = AudioSystem.getClip();
+		        AudioInputStream inputStream = AudioSystem.getAudioInputStream(
+		          Main.class.getResourceAsStream("Notes/" + str));
+		        clip.open(inputStream);
+		        clip.start(); 
+		      } catch (Exception e) {
+		        System.err.println(e.getMessage());
+		      }
+		    }
+		  }).start();
+		}
+	
+}
